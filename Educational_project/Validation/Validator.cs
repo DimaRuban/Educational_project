369cf29b5bd4294009461df494b0e3d6d89d3c8a
DimaRuban <dimkaruban@gmail.com>
2021-06-05 20:05:52 +0300

    исправил замечания.

--- Educational_project/Validation/Validator.cs
+++ Educational_project/Validation/Validator.cs
@@ -7,22 +7,17 @@ namespace StorePhone.Validation
     {
         public bool IsPhoneNumberValid(string message)
         {
-            string patternForPhoneNumber = @"\+?([0-9]{2})?[0]\(?[0-9]{2}\)?\s?[0-9]{3}\s?[0-9]{2}\s?[0-9]{2}";
-            var expressionForPhoneNumber = new Regex(patternForPhoneNumber, RegexOptions.Compiled);
-            var isMatch = expressionForPhoneNumber.IsMatch(message);
-
-            if (isMatch == true)
-                return true;
-            else return false;
+            var phoneNumberPattern = @"\+?([0-9]{2})?[0]\(?[0-9]{2}\)?\s?[0-9]{3}\s?[0-9]{2}\s?[0-9]{2}";
+            var expressionPhoneNumber = new Regex(phoneNumberPattern, RegexOptions.Compiled);
+           
+            return expressionPhoneNumber.IsMatch(message);
         }
         public bool IsHomeAddressValid(string message)
         {
-            string patternForHomeAdress = @"^(улица|ул\.)\s?[а-яA-Я]+(\.|,)\s?(дом|д\.)\s?[1-9]+(,\s?(квартира|кв\.)\s?[1-9]+)?$";
-            var expressionForHomeAdress = new Regex(patternForHomeAdress, RegexOptions.Compiled);
-            var isMatch = expressionForHomeAdress.IsMatch(message);
-            if (isMatch == true)
-                return true;
-            else return false;
+            var addressPattern = @"^(улица|ул\.)\s?[а-яA-Я]+(\.|,)\s?(дом|д\.)\s?[1-9]+(,\s?(квартира|кв\.)\s?[1-9]+)?$";
+            var expressionAddress = new Regex(addressPattern, RegexOptions.Compiled);
+            
+            return expressionAddress.IsMatch(message);
         }
     }
 }
