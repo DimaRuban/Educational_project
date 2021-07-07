0e2398c7e3db70618ebcf70b09675265322f25db
DimaRuban <dimkaruban@gmail.com>
2021-07-07 22:01:05 +0300

    added use of nameTable.

--- SQL/4/2.sql
+++ SQL/4/2.sql
@@ -1,19 +1,20 @@
-CREATE PROCEDURE CreateId (@tableName VARCHAR(10), @ID NVARCHAR(50) OUTPUT)
+CREATE PROCEDURE Create_Id (@tableName VARCHAR(10), @ID NVARCHAR(50) OUTPUT)
 AS
 BEGIN
-SELECT @ID = NEWID()
+SELECT @ID = SUBSTRING (@tableName, 0, 2) + '_' + CAST(NEWID() AS VARCHAR(50))
 END
 --------------------------------------------------------------------------
 DECLARE @IDi NVARCHAR(50)
 
-EXECUTE CreateId
+EXECUTE Create_Id
    @tableName = 'Products'
   ,@ID = @IDi OUTPUT
 
 INSERT INTO Products
 VALUES 
-(@IDi, 'Margherita', 90),
-(@IDi, 'Carbonara', 100),
-(@IDi, 'Quattro Stagioni', 110);
+--(@IDi, 'Margherita', 90),
+--(@IDi, 'Carbonara', 100),
+--(@IDi, 'Quattro Stagioni', 110);
+(@IDi, 'Montanara', 120);
 --------------------------------------------------------------------------
 SELECT * FROM Products
