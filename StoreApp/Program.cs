20b94cc197b2064952e72e5d2cf2c36e38c68fb0
DimaRuban <dimkaruban@gmail.com>
2021-07-23 00:44:39 +0300

    finished 4 tasks

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -10,6 +10,41 @@ namespace StoreApp
     {
         static void Main(string[] args)
         {
+            var dbContext = new DbContext();
+            dbContext.InitData();
+
+            Console.WriteLine("\n*****Task 1*****\n");
+
+            var productList1 = dbContext.Products.Select(x => x).OrderBy(x => x.Name);
+            foreach (var product in productList1)
+            {
+                Console.WriteLine($"Product: {product.Name}");
+            }
+
+            Console.WriteLine("\n*****Task 2*****\n");
+
+            var productList2 = dbContext.Products.Select(x => new { providerName = x.Provider.Name, productName = x.Name });
+            foreach (var product in productList2)
+            {
+                Console.WriteLine($"Provider - {product.providerName}, Product - {product.productName}");
+            }
+
+            Console.WriteLine("\n*****Task 3*****\n");
+
+            var productList3 = dbContext.Products.GroupBy(x => x.Category.Name).Select(x => new { categoryName = x.Key, count = x.Count() });
+            foreach (var product in productList3)
+            {
+                Console.WriteLine($"Category: {product.categoryName} - {product.count} products");
+            }
+
+            Console.WriteLine("\n*****Task 4*****\n");
+
+            var productList4 = dbContext.Products.GroupBy(x => x.Provider.Name).Select(x => new { providerName = x.Key, count = x.Count() }).OrderByDescending(x => x.count);
+            foreach (var product in productList4)
+            {
+                Console.WriteLine($"Provider: {product.providerName} - {product.count} products");
+            }
+
             Console.ReadKey();
         }
     }
