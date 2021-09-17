1702daa700a119da2a683769bd437c517c93a0cd
DimaRuban <dimkaruban@gmail.com>
2021-09-17 09:40:39 +0300

    fix

--- Educational_project/Data/CacheService.cs
+++ Educational_project/Data/CacheService.cs
@@ -8,16 +8,16 @@ namespace StorePhone.Data
 {
     public class CacheService : ICacheService
     {
+        public delegate void AddToCacheDelegate(Product product);
+
         private readonly Cache _cache;
         private static readonly object _locker = new object();
+        private AddToCacheDelegate _handler;
 
         public CacheService(Cache cache)
         {
             _cache = cache;
-        }
-
-        public CacheService()
-        {        
+            _handler = Add;
         }
 
         public IEnumerable<Product> Get()
@@ -42,11 +42,16 @@ namespace StorePhone.Data
                 if (_cache.DataCache.Count == 5)
                 {
                     _cache.DataCache.RemoveAt(0);
+                    _handler(product);
                 }
-                else
-                {
-                    _cache.DataCache.Add(product);
-                }
+               _handler(product);          
+            }
+        }
+        private void Add(Product product)
+        {
+            lock (_locker)
+            {
+                _cache.DataCache.Add(product);
             }
         }
     }
