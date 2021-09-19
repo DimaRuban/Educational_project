0a9240954fb15448e81979a6ab09e3a4966068dd
DimaRuban <58884154+DimaRuban@users.noreply.github.com>
2021-09-19 22:14:09 +0300

    Update Startup.cs

--- StorePhoneAPI/Startup.cs
+++ StorePhoneAPI/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using StorePhone.Controllers;
 using StorePhone.Services;
-using StorePhone.Ñontracts;
+using StorePhone.Contracts;
 
 namespace StorePhoneAPI
 {
